aeacdf858ced36d99972969ec80e4e01ab5a3098
Pako#4527 <pako.justin7@orange.fr>
2025-11-20 11:05:37 +0100

    class Penon

---
+++ app/src/main/java/com/example/apppenon/Penon.kt
@@ -0,0 +1,62 @@
+package com.example.apppenon
+import java.io.Serializable
+
+var penonName: String = "penon"
+var macAdress: String = "00:00:00:00:00:00"
+var rssi: Boolean = false
+var rssiLow: Int = -75
+var rssiHigh: Int = -50
+var flowState: Boolean = false
+var flowStateLow: Int = 500
+var flowStateHigh: Int = 800
+var sDFlowState: Boolean = false
+var sDFlowStateLow: Int = 200
+var sDFlowStateHigh: Int = 800
+var meanAcc: Boolean = false
+var meanAccLow: Int = 900
+var meanAccHigh: Int = 1200
+var sDAcc: Boolean = false
+var sDAccLow: Int = 6000
+var sDAccHigh: Int = 8000
+var maxAcc: Boolean = false
+var maxAccLow: Int = 1000
+var maxAccHigh: Int = 2000
+var vbat: Boolean = false
+var vbatLow: Double = 2.4
+var vbatHigh: Double = 2.8
+var detached: Boolean = false
+var detachedTresh: Double = 10.0
+var count: Boolean = false
+var ids: Boolean = false
+var timeline: Int = 0
+
+data class Penon (
+    var penonName: String = "penon",
+    var macAdress: String = "00:00:00:00:00:00",
+    var rssi: Boolean = false,
+    var rssiLow: Int = -75,
+    var rssiHigh: Int = -50,
+    var flowState: Boolean = false,
+    var flowStateLow: Int = 500,
+    var flowStateHigh: Int = 800,
+    var sDFlowState: Boolean = false,
+    var sDFlowStateLow: Int = 200,
+    var sDFlowStateHigh: Int = 800,
+    var meanAcc: Boolean = false,
+    var meanAccLow: Int = 900,
+    var meanAccHigh: Int = 1200,
+    var sDAcc: Boolean = false,
+    var sDAccLow: Int = 6000,
+    var sDAccHigh: Int = 8000,
+    var maxAcc: Boolean = false,
+    var maxAccLow: Int = 1000,
+    var maxAccHigh: Int = 2000,
+    var vbat: Boolean = false,
+    var vbatLow: Double = 2.4,
+    var vbatHigh: Double = 2.8,
+    var detached: Boolean = false,
+    var detachedThresh: Double = 10.0,
+    var count: Boolean = false,
+    var ids: Boolean = false,
+    var timeline: Int = 0
+): Serializable
